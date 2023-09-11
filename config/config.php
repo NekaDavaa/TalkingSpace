eb5783413b45431051bbc0e6780d52caa400f1c7
NekaDavaa <114173828+NekaDavaa@users.noreply.github.com>
2023-09-11 12:58:52 +0300

    db config

--- config/config.php
+++ config/config.php
@@ -2,8 +2,8 @@
 //DB Params
 define("DB_HOST", "localhost");
 define("DB_USER", "root");
-define("DB_PASS", "121212aa");
-define("DB_NAME", "talkingspace");
+define("DB_PASS", "");
+define("DB_NAME", "forum");
 
 define("SITE_TITLE", "Welcome To TalkingSpace!");
 
